bf8f62a7692651e532640fe7d2071d666da02e9d
Christian Well <christandubsky20@gmail.com>
2025-12-07 01:04:35 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { text } from "@sveltejs/kit";
+
+</script>
+<style> 
+    @import "./styles.css"
+</style>
+
+<h1>Rooted</h1>
+<img src="https://rooted.hackclub.com/orpheus_campfire_nobk.png" alt="orpheus type shit">
+<div style="text-align: center;">
+    <a href="src/routes/start/+page.svelte">enter the roots</a>   
+</div>
